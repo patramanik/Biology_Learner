95ed831468ad6e94bc9eb8fd1cee6146e5092aab
patramanik <patramanik409@gmail.com>
2023-12-23 19:44:11 +0530

    complict serch page

--- lib/Screens/Search/SearchPage.dart
+++ lib/Screens/Search/SearchPage.dart
@@ -1,20 +1,59 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, avoid_print
 
+import 'package:blology_learner/Screens/view/view.dart';
+import 'package:blology_learner/component/postItem.dart';
 import 'package:blology_learner/component/widgets/myDrawer.dart';
 import 'package:flutter/material.dart';
 
+import '../../Model/BodyPost.dart';
+import '../../services/homeBodyApi.dart';
+
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({Key? key}) : super(key: key);
 
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<BodyPostModel> searchPosts = [];
+  List<BodyPostModel> filteredPosts = [];
+
+  Future<void> fetchSearchPosts() async {
+    try {
+      List<BodyPostModel> response = await HomeBodyApi.getData();
+      setState(() {
+        searchPosts = response;
+        filteredPosts = response;
+      });
+    } catch (error) {
+      print('Error fetching body posts data: $error');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSearchPosts();
+  }
+
+  void _searchList(String value) {
+    setState(() {
+      if (value.isNotEmpty) {
+        filteredPosts = searchPosts
+            .where((post) =>
+                post.postName.toLowerCase().contains(value.toLowerCase()))
+            .toList();
+      } else {
+        filteredPosts = searchPosts;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-       appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         title: const Center(
           child: Text(
             "Search",
@@ -26,9 +65,61 @@ class _SearchPageState extends State<SearchPage> {
         ),
       ),
       drawer: const MyDrawer(),
-      // bottomNavigationBar: const MyBottemNave(),
-      body: const SizedBox(child: Center(child: Text("Search"))),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                onChanged: (value) {
+                  _searchList(value);
+                },
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  hintText: "Search",
+                  prefixIcon: const Icon(Icons.search),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 7,
+            child: Padding(
+              padding: const EdgeInsets.all(6),
+              child: ListView.builder(
+                itemCount: filteredPosts.length,
+                itemBuilder: (context, index) {
+                  final post = filteredPosts[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ViewPost(
+                              catagoryName: post.categoryName,
+                              postName: post.postName,
+                              postImages: post.image,
+                              content: post.postContent,
+                              mataTitle: post.metaTitle),
+                        ),
+                      );
+                    },
+                    child: PostItem(
+                        image: post.image,
+                        name: post.postName,
+                        matatitle: post.metaTitle,
+                        categoryName: post.categoryName),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
